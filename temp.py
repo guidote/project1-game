6a942b7bcb9edf980d0e09300aaa2c278cf4ad91
guidote <74155514+guidote@users.noreply.github.com>
2022-10-06 14:29:29 -0400

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,4 +1,86 @@
 # -*- coding: utf-8 -*-
-import pygame, sys
 #! pip install pygame
-print('hello world')
+
+
+'''
+#Pygame base template for opening a window
+ 
+import pygame
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("My Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+    # --- Game logic should go here
+ 
+    # --- Screen-clearing code goes here
+ 
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+ 
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(WHITE)
+ 
+    # --- Drawing code should go here
+ 
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+pygame.quit()
+
+'''
+import pygame, sys
+from pygame.locals import *
+
+def main():
+    pygame.init()
+    
+    width, height = 640, 480
+    screen = pygame.display.set_mode((width, height))
+    
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    
+    while True:
+        
+        screen.fill(WHITE)
+    
+        pygame.draw.rect(screen, BLACK, (200,150,100,50))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        
+main()
+    
+    
+    
