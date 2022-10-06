b4f1cef5818ae651fe3569da6a17caaf67bc8717
guidote <74155514+guidote@users.noreply.github.com>
2022-10-06 17:04:11 -0400

    Add files via upload

---
+++ background.py
@@ -0,0 +1,6 @@
+import pygame
+
+class Background:
+    def __init__(self, type_sprite):
+
+    def setSprite(self, type_sprite):  
--- duck.py
+++ duck.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  6 13:47:55 2022
+import pygame
 
-@author: gabrielleguidote
-"""
-
+class Player:
+    rightSprite = pygame.image.load('data/gfx/player.png')
+    leftSprite = pygame.transform.flip(rightSprite, True, False)
+    currentSprite = rightSprite
