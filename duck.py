fb3dff6da1fbf7866398d75028617716d34e6e3d
guidote <74155514+guidote@users.noreply.github.com>
2022-10-06 20:58:19 -0400

    images in duck.py

--- duck.py
+++ duck.py
@@ -1,6 +1,8 @@
 import pygame
 
 class Player:
-    rightSprite = pygame.image.load('data/gfx/player.png')
-    leftSprite = pygame.transform.flip(rightSprite, True, False)
-    currentSprite = rightSprite
+    
+    duck_right_imgs = ["waddle_right_0.png","waddle_right_1.png"]
+    duck_left_imgs = ["waddle_left_0.png","waddle_left_1.png"]
+    duck_front_imgs = ["waddle_front_0.png","waddle_front_1.png"]
+    duck_back_imgs = ["waddle_back_0.png","waddle_back_1.png"]
