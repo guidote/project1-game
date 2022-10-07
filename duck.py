45757ca27527cd427426efe2f7300a5638e9f563
guidote <74155514+guidote@users.noreply.github.com>
2022-10-07 19:25:16 -0400

    working on moving duck

--- duck.py
+++ duck.py
@@ -7,6 +7,15 @@ class Player:
     duck_front_imgs = ["waddle_front_0.png","waddle_front_1.png"]
     duck_back_imgs = ["waddle_back_0.png","waddle_back_1.png"]
     
-    def display_duck:
+    def display_duck(movement_state, mouse):
+        indices = [0,1]
+        if movement_state == pygame.MOUSEMOTION:
+            for i in indices:
+                duck = pygame.image.load(duck_front_imgs[i])
+                duck = pygame.transform.scale(duck, (duck.get_width()/7, duck.get_height()/7))
+                screen.blit(duck, (mouse[0] - (duck.get_width()/2) ,mouse[1] - (duck.get_width()/2)))
+                pygame.display.update()
+            
+        
         
         
