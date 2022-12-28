100f7c11394d3402525694b123efa3a03469bb58
guidote <74155514+guidote@users.noreply.github.com>
2022-12-28 02:01:24 -0500

    duck thing is kinda fixed

--- duck.py
+++ duck.py
@@ -10,31 +10,44 @@ class Duck:
     default_duck_img = pygame.image.load("data/gfx/waddle_front_0.png")
     width = default_duck_img.get_width()*5
     height = default_duck_img.get_height()*5
-    
-    '''
-    duck_right_imgs = ["waddle_right_0.png","waddle_right_1.png"]
-    duck_left_imgs = ["waddle_left_0.png","waddle_left_1.png"]
-    duck_front_imgs = ["waddle_front_0.png","waddle_front_1.png"]
-    duck_back_imgs = ["waddle_back_0.png","waddle_back_1.png"]
-    duck_idle_imgs = ["waddle_idle_0.png","waddle_idle_1.png"]
-    
-    '''
+
     def __init__(self):
-        pass
+        
+        self.images_right = []
+        self.images_left = []
+        self.images_front = []
+        self.images_back = []
+        self.images_idle = []
+        
+        self.images_right.append(pygame.image.load('data/gfx/waddle_right_0.png'))
+        self.images_right.append(pygame.image.load('data/gfx/waddle_right_1.png'))
+        
+        self.images_left.append(pygame.image.load('data/gfx/waddle_left_0.png'))
+        self.images_left.append(pygame.image.load('data/gfx/waddle_left_1.png'))
+        
+        self.images_front.append(pygame.image.load('data/gfx/waddle_front_0.png'))
+        self.images_front.append(pygame.image.load('data/gfx/waddle_front_1.png'))
+        
+        self.images_back.append(pygame.image.load('data/gfx/waddle_back_0.png'))
+        self.images_back.append(pygame.image.load('data/gfx/waddle_back_1.png'))
+        
+        self.images_idle.append(pygame.image.load('data/gfx/waddle_idle_0.png'))
+        self.images_idle.append(pygame.image.load('data/gfx/waddle_idle_1.png'))
+        
+        self.index = 0
+        
      
     def display_duck(self, mouse, screen, movement):
         if movement =='shop':
             pygame.display.update()
             pygame.time.delay(100)
         else:
-            indices = [0,1]
-            for i in indices:
-                img = pygame.image.load("data/gfx/waddle_{0}_{1}.png".format(movement, i))
-                img = pygame.Surface.convert_alpha(img)
-                duck = pygame.transform.scale(img, (self.width, self.height))
-                screen.blit(duck, (mouse[0] - (duck.get_width()/2) ,mouse[1] - (duck.get_height()/2)))
-                pygame.display.update()
-                pygame.time.delay(100)
+            img = movement[self.index]
+            img = pygame.Surface.convert_alpha(img)
+            duck = pygame.transform.scale(img, (self.width, self.height))
+            screen.blit(duck, (mouse[0] - (duck.get_width()/2) ,mouse[1] - (duck.get_height()/2)))
+            pygame.display.update()
+            pygame.time.delay(100)
     
     def check_pick_up(self, b_x, b_y, b_width, b_height):
         top = self.position.y + self.height/4
@@ -49,12 +62,21 @@ class Duck:
 
     def check_duck_position(self, previous_position_x, previous_position_y, position_x, position_y):
         if (position_x > previous_position_x and (position_x - previous_position_x)>10):
-            return "right"
+            return self.images_right
         elif (position_x < previous_position_x  and (previous_position_x - position_x)>10):
-            return "left"
+            return self.images_left
         elif (position_y > previous_position_y and (position_y - previous_position_y)>10):
-            return "front"
+            return self.images_front
         elif (position_y < previous_position_y and (previous_position_y - position_y)>10):
-            return "back"   
+            return self.images_back  
         else:
-            return "idle" # TODO : idle
+            return self.images_idle # TODO : idle
+        
+    def update(self, image_type):
+        self.index += 1
+        #if the index is larger than the total images
+        if self.index >= len(image_type):
+            #we will make the index to 0 again
+            self.index = 0
+
+        return image_type[self.index]
--- temp.py
+++ temp.py
@@ -133,10 +133,10 @@ def play():
                     duck.position.x, duck.position.y = mouse 
                     movement = duck.check_duck_position(previous_position_x, previous_position_y, mouse[0], mouse[1])
                     duck.display_duck(mouse, SCREEN, movement)
+                    duck.update(movement)
                 else:
                     # In Shop Area
                     duck.position.x, duck.position.y = mouse 
-
                     duck.display_duck(mouse, SCREEN, "shop")
 
                 pygame.display.update()
@@ -172,131 +172,7 @@ def play():
                     sys.exit()
                 
             pygame.display.update()
-    
-'''
-def main():
-    
-    # SET UP
-    pygame.init()
-    
-    game_state = "title_screen"
-    
-    #Pygame Window
-    width, height = 700, 600
-    SCREEN = pygame.display.set_mode((width, height))
-    
-    GRASS_GREEN = (145, 211, 109)
-    
-    #Create a duck
-    duck = Duck()
-    
-    # Getting Images for the shop
-    # shop = pygame.image.load('data/gfx/shop.png')
-    # weeds_button = pygame.image.load('data/gfx/weeds_button.png')
-    # flowers_button = pygame.image.load('data/gfx/flowers_button.png')
-    # worms_button = pygame.image.load('data/gfx/worms_button.png')
-    
-    # buttons = []
-    # buttons.append(Button(weeds_button))
-    # buttons.append(Button(flowers_button))
-    # buttons.append(Button(worms_button))
-
-    #Setting up weeds, flowers and worms
-    ground_objects = []
-    for i in range(5): ground_objects.append(GroundObject('weed'))
-    for i in range(3): ground_objects.append(GroundObject('flower'))
-    for i in range(2): ground_objects.append(GroundObject('worm'))
-
-    
-    #Assigning them a random starting position
-    # TODO: make a check to not generate two items on the same spot OR IN THE SHOP
-    for i in ground_objects:
-            randomx = random.randrange(SCREEN.get_width() - i.sprite.get_width())
-            randomy = random.randrange(SCREEN.get_height() - i.sprite.get_height())
-            i.position.xy = randomx, randomy
-       
-    # TITLE SCREEN
-    title_screen = Title_Screen()
-    while game_state == "title_screen":
-        
-        SCREEN.fill(GRASS_GREEN)
-        mouse = pygame.mouse.get_pos()
-        
-        title_screen.display_duck_title(SCREEN,mouse)
-        
-        for event in pygame.event.get():
-            # Mouse click start playing
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if title_screen.hover:
-                    game_state = "playing"
-                
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-
-        
-    # GAME LOOP
-    while game_state == "playing":
-        
-        SCREEN.fill(GRASS_GREEN)
-        # Display Shop and score section
-        # DISPLAY.blit(shop, (0, 0))
-
-        # for button in buttons:
-        #     DISPLAY.blit(button.sprite, (220 + (buttons.index(button)*125), 393))
-        #     priceDisplay = font_small.render(str(button.price), True, (0,0,0))
             
-        #     # For later development : LEVELS
-        #     #levelDisplay = font_20.render('Lvl. ' + str(button.level), True, (200,200,200))
-        #     #DISPLAY.blit(levelDisplay, (234 + (buttons.index(button)*125), 441))
-            
-        #     counter = font_small.render(str(button.count).zfill(7), True, (0,0,0))
-        #    #Below is 2 coordinates that have to depend on the button's position
-        #     #DISPLAY.blit(counter, (72, 394))
-
-        
-        #Display GroundObjects
-        for i in ground_objects:
-            SCREEN.blit(i.sprite, i.position.xy)
-            
-            
-        # Duck Previous Position
-        mouse = pygame.mouse.get_pos()
-        previous_position_x = mouse[0]
-        previous_position_y = mouse[1]
-        
-        #Event Handler
-        for event in pygame.event.get():
-            
-            # Mouse Moving Event = Move Duck
-            if event.type == pygame.MOUSEMOTION:
-                mouse = pygame.mouse.get_pos()
-                duck.position.x, duck.position.y = mouse 
-
-                movement = duck.check_duck_position(previous_position_x, previous_position_y, mouse[0], mouse[1])
-                duck.display_duck(mouse, SCREEN, movement)
-                pygame.display.update()
-                
-
-            #Check for Pick up and action of GroundObjects
-            for i in ground_objects:
-                if (duck.check_pick_up(i.position.x, i.position.y, i.sprite.get_width(), i.sprite.get_height())):
-                    # Counters
-                    #groud_objects[i].count +=1
-                    # Generating a new position
-                    randomx = random.randrange(SCREEN.get_width() - i.sprite.get_width())
-                    randomy = random.randrange(SCREEN.get_height() - i.sprite.get_height())
-                    i.position.xy = randomx, randomy
-        
-            #To Quit
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-                
-            pygame.display.update()
-'''
-        
 main_menu()
-    
     
     
